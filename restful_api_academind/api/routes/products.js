52774b8eeccfa18b41ad84dec7dd8ef443e8940b
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-26 17:00:38 +0100

    feat(add): orders endpoints, products pictures handling

--- restful_api_academind/api/routes/products.js
+++ restful_api_academind/api/routes/products.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './uploads/');
+  },
+  filename: function(req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, callback) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    callback(null, true); // accept file
+  } else {
+    callback(null, false); // reject file
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter
+ });
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            productImage: doc.productImage,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + doc._id
+            }
+          }
+        })
+      };
+      res.status(200).json(response);
     })
     .catch(err => {
       console.log(err);
@@ -19,18 +58,28 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+  console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product.save()
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: 'Handling POST requests to /products.',
-        createdProduct: result
+        message: 'Created product successfully.',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products/' + result._id
+          }
+        }
       });
     })
     .catch(err => {
@@ -45,11 +94,18 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
       console.log('From database ', doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products'
+          }
+        });
       } else {
         res.status(404).json({
           message: 'No valid entry found for provided ID.'
@@ -74,7 +130,13 @@ router.patch('/:productId', (req, res, next) => {
     .exec()
     .then(result => {
       console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: 'Product updated.',
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/products/' + id
+        }
+      });
     })
     .catch(err => {
       console.log(err);
@@ -89,7 +151,14 @@ router.delete('/:productId', (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then(result => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: 'Product deleted.',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/products',
+          body: { name: 'String', price: 'Number' }
+        }
+      });
     })
     .catch(err => {
       console.log(err);
